712c74acd783a1a93cad86e1f7b290b8c5ccbcd9
fengluxu <F.Xu-1@student.tudelft.nl>
2018-06-28 02:38:08 +0200

    bundleAjustment using lsqnonlin and cell2Matrix

---
+++ final/bundle_adjustment_function.m
@@ -0,0 +1,20 @@
+function E = bundle_adjustment_function(MS,point_matrix)%(threeDpoints,camera) 
+% camera: 6 x 3 x num_frame
+% threeDpoints: 3d points (3 x max_col x num_frame)
+% point_matrix: 3 2d points in images (6 x max_col x num_frame)
+% project:  6 x max_col x num_frame
+% Error to minimize
+E = [];
+
+[m n numImg] = size(point_matrix);
+
+for i = 1:numImg
+    MS_i = MS(:,:,i);
+    M = MS_i(1:m,:);
+    S = MS_i(m+1:end,:)';
+    error =  sum(sum( sqrt(M*S - point_matrix(:,:,i))));
+    E = [E,error];
+end 
+
+% then Using lsqnonlin Matlab function.
+end
